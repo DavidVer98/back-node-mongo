c69806aa9a67aab43c2fef1b8c84a6b43c4fa5a5
DavidVer98 <davidcuevas15@hotmail.com>
2022-12-13 20:49:16 -0300

    sync port

--- dist/index.js
+++ dist/index.js
@@ -6,5 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const database_1 = require("./database");
 (0, database_1.startConnection)(); // Call startConnection function
-app_1.default.listen(3000);
-console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app_1.default.listen(PORT);
+console.log(`Server runing on port ${PORT}`);
--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ import app from './app';
 import { startConnection } from './database';
 
 startConnection(); // Call startConnection function
-app.listen(3000)
-console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
+console.log(`Server runing on port ${PORT}`);
